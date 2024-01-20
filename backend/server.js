87e7c5d5c71fd055dac95fa37615d705457d699c
walz0 <aidan.m.walz@gmail.com>
2024-01-19 19:04:19 -0500

    notes for tresguerras y paquetexpress

--- backend/server.js
+++ backend/server.js
@@ -11,13 +11,51 @@ const he = require("he");
 
 const port = 3000;
 
-const empresas = [
-    "Tresguerras",
-    "Paquetexpress",
-    "Potosinos",
-    "Estafeta",
-    "Otro",
-];
+const paqueterias = {
+    "Tresguerras": "https://www.tresguerras.com.mx/3G/tracking.php#",
+    "Paquetexpress": "https://www.paquetexpress.com.mx/rastreo/",
+    "Potosinos": "https://paqueteriapotosinos.com.mx/potosinos-rastreo/",
+    "Estafeta": `https://cs.estafeta.com/es/Tracking/searchByGet?wayBill=${''}&wayBillType=1&isShipmentDetail=False`,
+    "Otro": undefined
+};
+
+/*
+{
+    "guia": "",
+    "embarcado": "",
+    "entregado": "",
+    "historia": {
+        {
+            "estado": "",
+            "sucursal": "",
+            "fecha": "",
+        },
+        ...
+    }
+}
+
+
+TRESGUERRAS
+POST https://www.tresguerras.com.mx/3G/assets/Ajax/tracking_Ajax.php
+url-encoded form
+    idTalon: CAN00150388
+    action: Talones
+    esKiosko: false
+
+PAQUETEXPRESS API
+
+https://cc.paquetexpress.com.mx/ptxws/rest/api/v1/guia/historico/ultimoevento/CJS01AA0287071/@1@2@3@4@5?source=WEBPAGE
+
+https://cc.paquetexpress.com.mx/ptxws/rest/api/v1/guia/historico/CJS01AA0287071/@1@2@3@4@5?source=WEBPAGE
+
+https://cc.paquetexpress.com.mx/ptxws/rest/api/v1/entrega/firma/CJS01AA0287071/@1@2@3@4@5?source=WEBPAGE
+
+https://cc.paquetexpress.com.mx/ptxws/rest/api/v1/entrega/acuse/CJS01AA0287071/@1@2@3@4@5?source=WEBPAGE
+
+https://cc.paquetexpress.com.mx/ptxws/rest/api/v1/sucursal/CJS01/@1@2@3@4@5?source=WEBPAGE
+
+https://cc.paquetexpress.com.mx/ptxws/rest/api/v1/sucursal/MEX03/@1@2@3@4@5?source=WEBPAGE
+*/
 
 app.use(express.static(path.join(__dirname, '/')));
 app.use(express.json());
@@ -70,23 +108,51 @@ app.get('/tresguerras', (req, res) => {});
 
 app.get('/paquetexpress', (req, res) => {});
 
-app.get('/estafeta', (req, res) => {
-    axios.get("https://cs.estafeta.com/es/Tracking/searchByGet?wayBill=3545762659&wayBillType=0&isShipmentDetail=False")
+app.get('/estafeta', async (req, res) => {
+    // get asp session id from cookie
+    // get tracking history with asp
+    // 
+
+    let cookie = undefined;
+    await axios.get("https://cs.estafeta.com/es/Tracking/searchByGet?wayBill=3545762659&wayBillType=0&isShipmentDetail=False")
         .then((response) => {
-            const history = parser.parse(response.data).querySelectorAll(".stateStatusDiv_4Items");
-            history.forEach((item) => {
-                const title = item.childNodes[1].getAttribute('title');
-                console.log(title);
-            });
+            // const history = parser.parse(response.data).querySelectorAll(".stateStatusDiv_4Items");
+            // history.forEach((item) => {
+            //     const title = item.childNodes[1].getAttribute('title');
+            //     console.log(title);
+            // });
             //res.send(he.decode(nodes.querySelector('.stateStatusDiv_4Items').innerHTML));
-            res.send(response.data);
+            // res.send(response.headers);
+            cookie = response.headers['set-cookie'].join('; ');
         })
         .catch((err) => {
             console.log(err);
-            res.sendStatus(500);
+            // res.sendStatus(500);
         });
+
+    console.log(cookie);
+
+    await axios({
+        method: 'POST',
+        url: 'https://cs.estafeta.com/es/Tracking/GetTrackingItemHistory',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Origin': 'https://cs.estafeta.com',
+            'cookie': cookie
+        },
+        data: {
+            "waybill": '501869766881C680191554'
+        }
+    }).then((response) => {
+        // console.log(response.data);
+        res.send(response.data);
+    }).catch((err) => {
+        // console.log(err);
+        res.sendStatus(500);
+    });
 });
 
+// ex: 3026756050
 app.post('/potosinos', (req, res) => {
     const guia = req.body.guia;
     console.log(guia);
