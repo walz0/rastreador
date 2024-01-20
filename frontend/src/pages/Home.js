5e9a1b3a203a783edc07ba6df90ba5a5ddfdf4c0
walz0 <aidan.m.walz@gmail.com>
2024-01-19 22:22:35 -0500

    ui updates

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -6,9 +6,13 @@ export default class Home extends Component {
     render() {
         return (
             <div className='Home'>
-                <h2 className='title'>Ordenes</h2>
-                <div className=''></div>
-                <Table cols={['Order ID', 'Date Ordered', 'Guía', 'Paquetería', 'Status', 'Rastrea']} />
+                <div className='Orders'>
+                    <h2 className='title'>Ordenes</h2>
+                    <Table cols={['Order ID', 'Date Ordered', 'Guía', 'Paquetería', 'Status', 'Rastrea']} />
+                </div>
+                <div className='Add'>
+                    <h2 className='title'>Agreger Orden</h2>
+                </div>
             </div>
         )
     }
